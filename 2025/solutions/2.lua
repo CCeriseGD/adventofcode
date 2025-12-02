b3fcae2c34183b1952815e5d7d0e2abfe31a53af
CCeriseGD <ccerisegd@gmail.com>
2025-12-02 16:28:56 +0100

    better

--- 2025/solutions/2.lua
+++ 2025/solutions/2.lua
@@ -1,19 +1,32 @@
 local total1 = 0
 local total2 = 0
+local found = {}
 for n1, n2 in input.gmatch("(%d+)%-(%d+)") do
-    for i = n1, n2 do
-        local str = tostring(i)
-        local invalid = false
-        for len = 1, #str/2 do
-            if #str % len == 0 then
-                if str == string.rep(str:sub(1, len), #str/len) then
-                    if not invalid then
-                        total2 = total2 + i
-                    end
-                    invalid = true
-                    if len == #str/2 then
-                        total1 = total1 + i
-                        break
+    local ranges = {}
+    if #n1 == #n2 then
+        table.insert(ranges, {tonumber(n1), tonumber(n2)})
+    else
+        table.insert(ranges, {tonumber(n1), 10^#n1-1})
+        table.insert(ranges, {10^#n1, tonumber(n2)})
+    end
+    for _, v in ipairs(ranges) do
+        local min, max = unpack(v)
+        local mins, maxs = tostring(min), tostring(max)
+        for len = 1, math.floor(#mins/2) do
+            if #mins/len % 1 == 0 then
+                local substmin = mins:sub(1, len)
+                local substmax = maxs:sub(1, len)
+                for subst = substmin, substmax do
+                    local repcount = #mins/len
+                    local rep = tonumber(string.rep(subst, repcount))
+                    if rep >= min and rep <= max then
+                        if repcount == 2 then
+                            total1 = total1 + rep
+                        end
+                        if not found[rep] then
+                            total2 = total2 + rep
+                            found[rep] = true
+                        end
                     end
                 end
             end
