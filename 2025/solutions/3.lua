ae71d64b6196034bbd6ba825d85a973c53944d48
CCeriseGD <ccerisegd@gmail.com>
2025-12-03 19:16:32 +0100

    slightly faster day 3

--- 2025/solutions/3.lua
+++ 2025/solutions/3.lua
@@ -1,21 +1,29 @@
-local function findlargest(str, numleft)
-    local patternend = numleft == 0 and "" or "("..string.rep(".", numleft).."+)"
-    for i = 9, 1, -1 do
-        local left = str:match("^.-"..i..patternend)
-        if left then
-            if numleft == 0 then
-                return i
+local function findlargest(str, len, highest)
+    local res = 0
+    local last = 0
+    for i = 1, len do
+        local maxn, maxpos = 0, 0
+        for pos = last + 1, #str - len + i do
+            local n = tonumber(str:sub(pos, pos))
+            if n > maxn then
+                maxn = n
+                maxpos = pos
+                if n == 9 then
+                    break
+                end
             end
-            return i * 10^numleft + findlargest(left, numleft-1)
         end
+        res = res * 10 + maxn
+        last = maxpos
     end
+    return res
 end
 
 local total1 = 0
 local total2 = 0
 for line in input.lines() do
-    total1 = total1 + findlargest(line, 1)
-    total2 = total2 + findlargest(line, 11)
+    total1 = total1 + findlargest(line, 2)
+    total2 = total2 + findlargest(line, 12)
 end
 output.part1(total1)
 output.part2(total2)
